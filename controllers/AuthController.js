12eab187b148432483b37a5aa886e88dccfd2e08
MercyNJ <mercinjuguna@gmail.com>
2024-02-09 02:02:37 -0800

    Authentication controller added

---
+++ controllers/AuthController.js
@@ -0,0 +1,59 @@
+import dbClient from '../utils/db';
+import { v4 as uuidv4 } from 'uuid';
+import redisClient from '../utils/redis';
+import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
+
+const AuthController = {
+  getConnect: async (req, res) => {
+    try {
+      const authHeader = req.headers.authorization;
+      if (!authHeader || !authHeader.startsWith('Basic ')) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const credentials = Buffer.from(authHeader.substring(6), 'base64').toString().split(':');
+      const [email, password] = credentials;
+
+      if (!email || !password) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const user = await dbClient.usersCollection.findOne({ email, password: sha1(password) });
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const token = uuidv4();
+      await redisClient.set(`auth_${token}`, user._id.toString(), 86400);
+
+      res.status(200).json({ token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  getDisconnect: async (req, res) => {
+    try {
+      const { 'x-token': token } = req.headers;
+      if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const userId = await redisClient.get(`auth_${token}`);
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      await redisClient.del(`auth_${token}`);
+
+      res.status(204).send();
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+};
+
+export default AuthController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 
 
 const UsersController = {
@@ -27,6 +29,29 @@ const UsersController = {
       });
 
       res.status(201).json({ id: newUser.insertedId, email });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  getMe: async (req, res) => {
+    try {
+      const { 'x-token': token } = req.headers;
+      if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const userId = await redisClient.get(`auth_${token}`);
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const user = await dbClient.usersCollection.findOne({ _id: ObjectId(userId) });
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+      res.status(200).json({ id: user._id, email: user.email });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
