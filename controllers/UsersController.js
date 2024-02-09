ef52e260b83a44bee99481683204204d8ef9c725
MercyNJ <mercinjuguna@gmail.com>
2024-02-09 01:14:22 -0800

    UsersController added

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,7 +7,6 @@ const UsersController = {
     try {
       const { email, password } = req.body;
 
-      // Check if email and password are provided
       if (!email) {
         return res.status(400).json({ error: 'Missing email' });
       }
@@ -15,22 +14,18 @@ const UsersController = {
         return res.status(400).json({ error: 'Missing password' });
       }
 
-      // Check if email already exists in the database
       const existingUser = await dbClient.usersCollection.findOne({ email });
       if (existingUser) {
         return res.status(400).json({ error: 'Already exist' });
       }
 
-      // Hash the password using SHA1
       const hashedPassword = sha1(password).toString('hex');
 
-      // Create a new user in the database
       const newUser = await dbClient.usersCollection.insertOne({
         email,
-        password: hashedPassword // Store hashed password
+        password: hashedPassword
       });
 
-      // Return the new user with only the email and the id
       res.status(201).json({ id: newUser.insertedId, email });
     } catch (error) {
       console.error(error);
